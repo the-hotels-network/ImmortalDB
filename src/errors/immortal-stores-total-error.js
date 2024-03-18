9ac3c40746d9d5b516043f8cdcd1cf45ba3b32c9
Carles Escrig Royo <ce@thehotelsnetwork.com>
2024-03-18 15:06:16 +0100

    fix: typo in error class name

--- src/errors/immortal-stores-total-error.js
+++ src/errors/immortal-stores-total-error.js
@@ -3,6 +3,6 @@ import { ImmortalError } from './immortal-error';
 export class ImmortalStoresTotalError extends ImmortalError {
     constructor(message = 'All stores failed to perform operation') {
         super(message);
-        this.name = 'ImmortalStoresPartialError';
+        this.name = 'ImmortalStoresTotalError';
     }
 }
