4c6515dc3e5b7a604876e9fb9605faaa9cdae47c
Carles Escrig Royo <ce@thehotelsnetwork.com>
2024-09-03 21:48:10 +0200

    feat!: remove cookie store from defaults

--- src/defaults.js
+++ src/defaults.js
@@ -1,11 +1,10 @@
-import { CookieStore } from './cookie-store';
 import { IndexedDbStore } from './indexed-db';
 import { LocalStorageStore } from './web-storage';
 import { getGlobal } from './helpers';
 
 export const DEFAULT_VALUE = undefined;
 export const DEFAULT_KEY_PREFIX = '_immortal|';
-export const DEFAULT_STORES = [CookieStore];
+export const DEFAULT_STORES = [];
 
 const window = /* #__PURE__ */ getGlobal();
 
